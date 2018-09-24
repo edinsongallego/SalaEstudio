9e2f7e7deea7ec3216e6c3fad62fa12858d6c139
oscar_mesa <oscarmesa.elpoli@gmail.com>
2018-09-24 18:50:08 -0500

    Merge origin/master
    Conflicts:
    	config/db.php
    	js/ventas.js
    	ventas.php
    
    Cambios en el modulo de creacion de banda

--- js/banda.js
+++ js/banda.js
@@ -22,6 +22,9 @@ $(document).ready(function () {
         },
     });
 
+    crearSelectIntegrantes();
+    crearSelectEditLider();
+
     $("#id_lider").select2({
         allowClear: true,
         placeholder: "Seleccione el lider de la banda",
@@ -33,7 +36,7 @@ $(document).ready(function () {
                 term = params.term;
                 var query = {
                     search: params.term,
-                    id_usuarios_existentes:$('#id_integrantes').val(),
+                    id_usuarios_existentes: $('#id_integrantes').val(),
                     page: params.page || 1
                 }
 
@@ -45,67 +48,95 @@ $(document).ready(function () {
             }
         },
     });
-    
-    $("#btn_nueva_banda").click(function(e){
+
+    $("#btn_nueva_banda").click(function (e) {
         e.preventDefault();
         $.fn.modal.Constructor.prototype.enforceFocus = $.noop;
         $("#newBanda").modal("toggle");
     });
-    
-    $("#btn_cancelar").click(function(e){
+
+    $("#btn_cancelar").click(function (e) {
         $("#newBanda").modal("toggle");
     });
 
-    $("#editar_banda").submit(function (event) {
-        $('#actualizar_datos2').attr("disabled", true);
+    $("#btn_cancelar_edit").click(function (e) {
+        e.preventDefault();
+        $("#editarbanda").modal("toggle");
+    });
 
-        var parametros = $(this).serialize();
-        $.ajax({
-            type: "POST",
-            url: "ajax/editar_banda.php",
-            data: parametros,
-            beforeSend: function (objeto) {
-                $("#resultados_editar").html("Mensaje: Cargando...");
-            },
-            success: function (datos) {
-                $("#resultados_editar").html(datos);
-                $('#actualizar_datos2').attr("disabled", false);
-                load(1);
-                setTimeout(function () {
-                    $("#editarbanda").modal('toggle');
-                }, 2000);
-            }
-        });
+
+    $("#editar_banda").submit(function (event) {
+
+        if ($("#editar_banda")[0].checkValidity()) {
+            $('#actualizar_datos2').attr("disabled", true);
+            $("#loading").toggle();
+            var parametros = $(this).serialize();
+            $.ajax({
+                type: "POST",
+                url: "ajax/editar_banda.php",
+                data: parametros,
+                beforeSend: function (objeto) {
+                    $("#resultados_editar").html("Mensaje: Cargando...");
+                },
+                success: function (datos) {
+                    $("#loading").toggle();
+                    $("#resultados_editar").html(datos);
+                    $('#actualizar_datos2').attr("disabled", false);
+                    load(1);
+                    setTimeout(function () {
+                        $("#editarbanda").modal('toggle');
+                    }, 2000);
+                }
+            });
+        }
         event.preventDefault();
     });
 
     $("#guardar_banda2").submit(function (event) {
-        $('#guardar_datos_banda').attr("disabled", true);
 
-        var parametros = $(this).serialize();
-        $.ajax({
-            type: "POST",
-            url: "ajax/nuevo_banda.php",
-            data: parametros,
-            beforeSend: function (objeto) {
-                $("#resultados_ajax_banda2").html("Mensaje: Cargando...");
-            },
-            success: function (datos) {
-                $("#resultados_ajax_banda2").html(datos);
-                $('#guardar_datos_banda').attr("disabled", false);
-                $("#guardar_banda2")[0].reset();
-                load(1);
-                setTimeout(function () {
-                    $("#newBanda").modal('toggle');
-                }, 2000);
-            }
-        });
+        if ($("#guardar_banda2")[0].checkValidity()) {
+            $('#guardar_datos_banda').attr("disabled", true);
+            $("#loading").toggle();
+            var parametros = $(this).serialize();
+            $.ajax({
+                type: "POST",
+                url: "ajax/nuevo_banda.php",
+                data: parametros,
+                beforeSend: function (objeto) {
+                    $("#resultados_ajax_banda2").html("Mensaje: Cargando...");
+                },
+                success: function (datos) {
+                    $("#resultados_ajax_banda2").html(datos);
+                    $("#loading").toggle();
+                    $('#guardar_datos_banda').attr("disabled", false);
+                    limpiar_formulario();
+                    load(1);
+                    setTimeout(function () {
+                        $("#newBanda").modal('toggle');
+                    }, 2000);
+                }
+            });
+        }
         event.preventDefault();
 
     });
 
     load(1);
 });
+
+
+function limpiar_formulario() {
+    $("#guardar_banda2")[0].reset();
+    $("#id_lider").empty().trigger('change');
+    $("#id_integrantes").empty().trigger('change');
+}
+
+function limpiar_formulario_ed() {
+    $("#editar_banda")[0].reset();
+    $("#id_lider_ed").empty().trigger('change');
+    $("#id_integrantes_ed").empty().trigger('change');
+    $("#resultados_editar").html("");
+}
 
 function load(page) {
     var q = $("#q").val();
@@ -144,18 +175,107 @@ function eliminar(id)
     }
 }
 
+function crearSelectIntegrantes() {
+    $('#id_integrantes_ed').select2({
+        allowClear: false,
+        language: "es",
+        placeholder: "Integrantes de la banda",
+        multiple: true,
+        ajax: {
+            url: 'ajax/autocomplete/usuarios_banda.php',
+            cache: "true",
+            type: 'POST',
+            dataType: 'json',
+            data: function (data, page) {
+                data["id_usuarios_existentes"] = $("#id_lider_ed").val();
+                data["search"] = data.term;
+                return data;
+            },
+            results: function (data, page) {
+                return {results: data.results};
+            }
+        },
+    });
+
+}
+
+function crearSelectEditLider(data) {
+
+    $("#id_lider_ed").select2({
+        allowClear: true,
+        language: "es",
+        placeholder: "Seleccione el lider de la banda",
+        data: processData(data).results,
+        // selectOnClose: true,
+        processResults: function (data) {
+            console.log(data);
+            return {
+                results: data.users,
+                pagination: {
+                    more: data.next !== null
+                }
+            };
+
+        },
+        ajax: {
+            cache: true,
+            url: 'ajax/autocomplete/usuarios_banda.php',
+            dataType: 'json',
+            data: function (params) {
+                term = params.term;
+                var query = {
+                    search: params.term,
+                    id_usuarios_existentes: $('#id_integrantes_ed').val(),
+                    page: params.page || 1
+                }
+
+                // Query parameters will be ?search=[term]&page=[page]
+                return query;
+            },
+            results: function (data, page) {
+                return {results: data.results};
+            }
+        },
+    });
+
+    // $('#id_lider_ed').val(selected).trigger('change');
+}
+
+function processData(data) {
+    var mapdata = $.map(data, function (obj) {
+        return obj;
+    });
+    return {results: mapdata};
+}
+
 function obtener_datos_banda(id) {
     var nombre_banda = $("#DS_NOMBRE_BANDA" + id).val();
     var descripcion_banda = $("#DS_DESCRIPCION_BANDA" + id).val();
     var id_banda = $("#DS_DESCRIPCION_BANDA" + id).val();
-
-    console.log("nombre banda:" + nombre_banda);
-    console.log("descripcion banda:" + descripcion_banda);
+    limpiar_formulario_ed();
 
     $("#nombrebanda").val(nombre_banda);
     $("#descripcionbanda").val(descripcion_banda);
     $("#mod_id_banda").val(id);
 
+    $.get("ajax/obtener_datos_banda.php", {id_banda: id}, function (data) {
+        var inte = [];
+        $.when($.each(data.integrantes, function (i, integrante) {
+            if (integrante.ES_LIDER == 1) {
+                $("#id_lider_ed").select2("trigger", "select", {
+                    data: {id: integrante.NM_DOCUMENTO_ID, text: integrante.DS_NOMBRE_USUARIO, modelo: integrante}
+                });
+            } else {
+                inte.push(integrante);
+                $("#id_integrantes_ed").select2("trigger", "select", {
+                    data: {id: integrante.NM_DOCUMENTO_ID, text: integrante.DS_NOMBRE_USUARIO, modelo: integrante}
+                });
 
+            }
+        })).then(function () {
+            // crearSelectIntegrantes(inte);
+        });
+    }, "JSON");
+    $.fn.modal.Constructor.prototype.enforceFocus = $.noop;
 }
 
--- modal/editar_banda.php
+++ modal/editar_banda.php
@@ -9,32 +9,43 @@ if (isset($con)) {
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Editar banda</h4>
                 </div>
-                <div class="modal-body">
-                    <form class="form-horizontal" method="post" id="editar_banda" name="editar_banda"autocomplete="off">
+                <form class="form-horizontal" method="post" id="editar_banda" name="editar_banda">
+                    <div class="modal-body">
                         <div id="resultados_editar"></div>
 
                         <div class="form-group row">
                             <label for="firstname" class="col-sm-3 control-label">*Nombre Banda</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="nombrebanda" name="nombrebanda" placeholder="*Nombre Banda" class="form-control" >
+                                <input type="text" required="" class="form-control" id="nombrebanda" name="nombrebanda" placeholder="*Nombre Banda" class="form-control" >
                                 <input type="hidden" id="mod_id_banda" name="mod_id_banda">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="lastname" class="col-sm-3 control-label">*Descripción</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control" id="descripcionbanda" name="descripcionbanda" placeholder="*Descripción Banda" class="form-control" >
+                                <input type="text" required="" class="form-control" id="descripcionbanda" name="descripcionbanda" placeholder="*Descripción Banda" class="form-control" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="firstname" class="col-sm-3 control-label">*Lider banda</label>
+                            <div class="col-sm-8">
+                                <select name="id_lider_banda" required="required" id="id_lider_ed" style="width:100%"></select>
                             </div>
                         </div>
 
 
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-8">
-                        <button type="submit" class="btn btn-primary float-right" id="guardar_datos_banda">Guardar</button>
+                        <div class="form-group">
+                            <label for="firstname" class="col-sm-3 control-label">*Integrantes de la banda</label>
+                            <div class="col-md-8" id="ctn_id_integrantes">
+                                <select required="required" style="width:100%" name="integrantes[]" class="form-control" id="id_integrantes_ed"></select>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <div class="modal-footer" style="text-align: center;">
+                        <button id="btn_cancelar_edit" class="btn btn-danger">Cancelar</button>
+                        <button type="submit" class="btn btn-primary float-right" id="guardar_datos_banda_edit">Guardar</button>                    
+                    </div>
 
                 </form>
             </div>
